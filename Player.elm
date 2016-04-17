eef4ee6c0f2f2d382c8cc83d5321aa99e0b836ea
Samuel Gélineau <gelisam+github@gmail.com>
2016-04-17 00:30:14 -0400

    level design

--- Player.elm
+++ Player.elm
@@ -33,7 +33,7 @@ init start_coord =
   -- , shape = I
   -- , orientation = R0
   -- , powerupIds = Set.empty
-  , shape = I
+  , shape = T
   , orientation = R0
   , powerupIds = Set.fromList <| List.map Powerup.id [Jump]
   }
